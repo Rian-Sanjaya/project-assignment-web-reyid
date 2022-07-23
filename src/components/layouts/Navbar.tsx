e016ade81f48a3328d9d322bc771e50b37098320
Rian Sanjaya <rian.sj@gmail.com>
2022-07-23 10:45:57 +0700

    add link active

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,8 +1,11 @@
 import React, { FC } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { Box } from "@material-ui/core";
 
 const TopNavbar: FC = () => {
+  const router = useRouter();
+
   return (
     <Box component="div"  pl={10} pr={10} pt={1} pb={1} style={{ backgroundColor: '#FFF', display: 'flex', flexWrap: 'wrap' }}>
       <div style={{ display: 'inline-block', width: '167px', marginRight: '40px' }}>
@@ -10,12 +13,32 @@ const TopNavbar: FC = () => {
       </div>
       <div style={{ margin: 'auto 0', textAlign: 'center', marginRight: '40px' }}>
         <Link href="/pokemon">
-          <a style={{ textDecoration: 'none', fontSize: '16px', lineHeight: '24px', color: '#42494D' }}>Home</a>
+          <a 
+            style={{ 
+              textDecoration: 'none', 
+              fontSize: '16px', 
+              lineHeight: '24px', 
+              color: `${router.pathname === "/pokemon" ? "#E6AB09" : "#42494D"}`,
+              fontWeight: `${router.pathname === "/pokemon" ? "700" : "normal"}`,
+            }}
+          >
+            Home
+          </a>
         </Link>
       </div>
       <div style={{ margin: 'auto 0', textAlign: 'center' }}>
         <Link href="/pokemon/type/1">
-          <a style={{ textDecoration: 'none', fontSize: '16px', lineHeight: '24px', color: '#42494D' }}>Pokemon Type</a>
+          <a 
+            style={{ 
+              textDecoration: 'none', 
+              fontSize: '16px', 
+              lineHeight: '24px', 
+              color: `${router.pathname === "/pokemon/type/1" ? "#E6AB09" : "#42494D"}`,
+              fontWeight: `${router.pathname === "/pokemon/type/1" ? "700" : "normal"}`,
+            }}
+          >
+            Pokemon Type
+          </a>
         </Link>
       </div>
     </Box>
